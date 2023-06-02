f4c7c064e95c082a5758c3f0b2b36f8494d6541c
Fotios <fotios.kornarakis@copper.co>
2023-06-02 11:12:00 +0100

    v0.21.0-scala2.13ar

--- test/src/test/scala/co/copperexchange/arweave4s/adt/Base64EncodedBytesSpec.scala
+++ test/src/test/scala/co/copperexchange/arweave4s/adt/Base64EncodedBytesSpec.scala
@@ -17,7 +17,7 @@ class Base64EncodedBytesSpec extends WordSpec with Matchers with ScalaCheckDrive
 
     "support inequality" in {
       forAll { (a: Array[Byte], b: Array[Byte]) =>
-        whenever(a.sameElements(b)) {
+        whenever(!a.sameElements(b)) {
           new Foo(a) should not be (new Foo(b))
         }
       }
